55f4c64e5fe3c2da79630fc48583650e3e44484b
Rex Jolley <emulsifier@fastmail.fm>
2015-01-25 22:56:57 -0700

    Adding a few new features to the eclipse version that were added to android studio version

--- src/main/java/com/kr4/simplenetworking/JSONNetworkLoader.java
+++ src/main/java/com/kr4/simplenetworking/JSONNetworkLoader.java
@@ -13,90 +13,100 @@ import com.kr4.simplenetworking.utils.NetworkUtils;
 
 public class JSONNetworkLoader extends NetworkLoader<JSONNetworkListener, JSONNetworkListenerContainer> {
 
-	private static JSONNetworkLoader instance;
+    private static JSONNetworkLoader instance;
 
-	public static JSONNetworkLoader getInstance() {
-		if (instance == null) {
-			instance = new JSONNetworkLoader();
-		}
-		return instance;
-	}
+    public static JSONNetworkLoader getInstance() {
+        if (instance == null) {
+            instance = new JSONNetworkLoader();
+        }
+        return instance;
+    }
 
-	public void executeGet(final String containerTag, final String requestID, final String url, final Type responseType) {
-		Log.i("SimpleNetworking", "Getting url: " + url);
-		setInProgressRequestIDForTag(containerTag, requestID);
-		AsyncTask<Type, Void, Object> task = new AsyncTask<Type, Void, Object>() {
-			Type responseType;
+    public void executeGet(final String containerTag, final String requestID, final String url) {
+        executeGet(containerTag, requestID, url, new TypeToken<Object>() {
+        }.getType());
+    }
 
-			@Override
-			protected void onPreExecute() {
+    public void executeGet(final String containerTag, final String requestID, final String url, final Type responseType) {
+        Log.i("SimpleNetworking", "Getting url: " + url);
+        setInProgressRequestIDForTag(containerTag, requestID);
+        AsyncTask<Type, Void, Object> task = new AsyncTask<Type, Void, Object>() {
+            Type responseType;
 
-			}
+            @Override
+            protected void onPreExecute() {
 
-			@Override
-			protected Object doInBackground(Type... parms) {
-				responseType = parms[0];
-				Object result = NetworkUtils.executeGet(url, responseType);
-				return result;
-			}
+            }
 
-			@Override
-			protected void onPostExecute(Object result) {
-				replyToListeners(containerTag, requestID, result);
-			}
-		};
-		task.execute(responseType);
-	}
+            @Override
+            protected Object doInBackground(Type... parms) {
+                responseType = parms[0];
+                Object result = NetworkUtils.executeGet(url, responseType);
+                return result;
+            }
 
-	public void executePost(final String containerTag, final String requestID, final String url, final Object postData) {
-		executePost(containerTag, requestID, url, postData, new TypeToken<Object>() {
-		}.getType());
-	}
+            @Override
+            protected void onPostExecute(Object result) {
+                replyToListeners(containerTag, requestID, result);
+            }
+        };
+        task.execute(responseType);
+    }
 
-	public void executePost(final String containerTag, final String requestID, final String url, final Object postData, final Type responseType) {
-		setInProgressRequestIDForTag(containerTag, requestID);
-		AsyncTask<Type, Void, Object> task = new AsyncTask<Type, Void, Object>() {
+    public void executePost(final String containerTag, final String requestID, final String url, final Object postData) {
+        executePost(containerTag, requestID, url, postData, new TypeToken<Object>() {
+        }.getType());
+    }
 
-			@Override
-			protected void onPreExecute() {
+    public void executePost(final String containerTag, final String requestID, final String url, final Object postData, final Type responseType) {
+        setInProgressRequestIDForTag(containerTag, requestID);
+        AsyncTask<Type, Void, Object> task = new AsyncTask<Type, Void, Object>() {
 
-			}
+            @Override
+            protected void onPreExecute() {
 
-			@Override
-			protected Object doInBackground(Type... parms) {
-				Type responseType = parms[0];
-				String jsonString = NetworkUtils.executePost(url, postData);
-				Gson gson = new Gson();
-				Object jsonObject = gson.fromJson(jsonString, responseType);
-				return jsonObject;
-			}
+            }
 
-			@Override
-			protected void onPostExecute(Object result) {
-				replyToListeners(containerTag, requestID, result);
-			}
-		};
-		task.execute(responseType);
-	}
+            @Override
+            protected Object doInBackground(Type... parms) {
+                Type responseType = parms[0];
+                String jsonString = NetworkUtils.executePost(url, postData);
+                Gson gson = new Gson();
+                try {
+                    Object jsonObject = gson.fromJson(jsonString, responseType);
+                    return jsonObject;
+                } catch (Exception e) {
+                    Log.i("JSONNetworkLoader", "Error parsing json response: " + jsonString);
+                    return null;
+                }
+            }
 
-	protected void replyToListeners(String containerTag, String requestID, Object data) {
-		clearInProgressReuqestIDForTag(containerTag, requestID);
-		JSONNetworkListenerContainer container = getListenerContainer(containerTag);
-		if (container != null) {
-			container.replyToListeners(requestID, data);
-		}
-	}
+            @Override
+            protected void onPostExecute(Object result) {
+                replyToListeners(containerTag, requestID, result);
+            }
+        };
+        task.execute(responseType);
+    }
 
-	protected void replyToListenersWithError(String containerTag, String requestID, String error) {
-		clearInProgressReuqestIDForTag(containerTag, requestID);
-		JSONNetworkListenerContainer container = getListenerContainer(containerTag);
-		if (container != null) {
-			container.replyToListenersWithError(requestID, error);
-		}
-	}
+    protected void replyToListeners(String containerTag, String requestID, Object data) {
+        clearInProgressReuqestIDForTag(containerTag, requestID);
+        JSONNetworkListenerContainer container = getListenerContainer(containerTag);
+        if (container != null) {
+            container.replyToListeners(requestID, data);
+        }
+    }
 
-	@Override
-	protected JSONNetworkListenerContainer newContainer(String tag) {
-		return new JSONNetworkListenerContainer(tag);
-	}
+    protected void replyToListenersWithError(String containerTag, String requestID, String error) {
+        clearInProgressReuqestIDForTag(containerTag, requestID);
+        JSONNetworkListenerContainer container = getListenerContainer(containerTag);
+        if (container != null) {
+            container.replyToListenersWithError(requestID, error);
+        }
+    }
+
+    @Override
+    protected JSONNetworkListenerContainer newContainer(String tag) {
+        return new JSONNetworkListenerContainer(tag);
+    }
 }
--- src/main/java/com/kr4/simplenetworking/utils/NetworkUtils.java
+++ src/main/java/com/kr4/simplenetworking/utils/NetworkUtils.java
@@ -22,128 +22,128 @@ import com.google.gson.Gson;
 
 public class NetworkUtils {
 
-	public static final int IO_BUFFER_SIZE = 1024;
+    public static final int IO_BUFFER_SIZE = 1024;
 
-	public static String executeGet(String getURL) {
-		HttpURLConnection urlConnection = null;
-		try {
-			URL url = new URL(getURL);
-			urlConnection = (HttpURLConnection) url.openConnection();
-			urlConnection.addRequestProperty("Accept", "application/json");
+    public static String executeGet(String getURL) {
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(getURL);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.addRequestProperty("Accept", "application/json");
 
-			InputStream in = new BufferedInputStream(
-					urlConnection.getInputStream());
-			InputStreamReader isr = new InputStreamReader(in);
-			BufferedReader br = new BufferedReader(isr);
-			String line = "";
-			String json = "";
-			while ((line = br.readLine()) != null) {
-				json += line;
-			}
-			return json;
-		} catch (Exception e) {
-			e.printStackTrace();
-			Log.i("NetworkUtils",
-					"Exception executing request: " + e.getMessage());
-		} finally {
-			if (urlConnection != null) {
-				urlConnection.disconnect();
-			}
-		}
-		return null;
-	}
+            InputStream in = new BufferedInputStream(
+                    urlConnection.getInputStream());
+            InputStreamReader isr = new InputStreamReader(in);
+            BufferedReader br = new BufferedReader(isr);
+            String line = "";
+            String json = "";
+            while ((line = br.readLine()) != null) {
+                json += line;
+            }
+            return json;
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.i("NetworkUtils",
+                    "Exception executing request: " + e.getMessage());
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+        return null;
+    }
 
-	public static Object executeGet(String getURL, Type type) {
-		HttpURLConnection urlConnection = null;
-		try {
-			URL url = new URL(getURL);
-			urlConnection = (HttpURLConnection) url.openConnection();
-			urlConnection.addRequestProperty("Accept", "application/json");
+    public static Object executeGet(String getURL, Type type) {
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(getURL);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.addRequestProperty("Accept", "application/json");
 
-			InputStream in = new BufferedInputStream(
-					urlConnection.getInputStream());
-			InputStreamReader isr = new InputStreamReader(in);
-			Gson gson = new Gson();
-			Object result = gson.fromJson(isr, type);
-			return result;
-		} catch (Exception e) {
-			Log.i("NetworkUtils", "Exception creating user");
-		} finally {
-			if (urlConnection != null) {
-				urlConnection.disconnect();
-			}
-		}
-		return null;
-	}
+            InputStream in = new BufferedInputStream(
+                    urlConnection.getInputStream());
+            InputStreamReader isr = new InputStreamReader(in);
+            Gson gson = new Gson();
+            Object result = gson.fromJson(isr, type);
+            return result;
+        } catch (Exception e) {
+            Log.i("NetworkUtils", "Exception creating user");
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+        return null;
+    }
 
-	public static String executePost(String postURL, Object postData) {
-		try {
-			Gson gson = new Gson();
+    public static String executePost(String postURL, Object postData) {
+        try {
+            Gson gson = new Gson();
 
-			String postJson = gson.toJson(postData);
+            String postJson = gson.toJson(postData);
 
-			HttpClient httpclient = new DefaultHttpClient();
-			httpclient.getParams().setParameter(
-					CoreConnectionPNames.CONNECTION_TIMEOUT, 10000);
-			httpclient.getParams().setParameter(
-					CoreConnectionPNames.SO_TIMEOUT, 20000);
-			HttpPost httppost = new HttpPost(postURL);
+            HttpClient httpclient = new DefaultHttpClient();
+            httpclient.getParams().setParameter(
+                    CoreConnectionPNames.CONNECTION_TIMEOUT, 10000);
+            httpclient.getParams().setParameter(
+                    CoreConnectionPNames.SO_TIMEOUT, 20000);
+            HttpPost httppost = new HttpPost(postURL);
 
-			StringEntity strEntity = new StringEntity(postJson);
-			httppost.setEntity(strEntity);
+            StringEntity strEntity = new StringEntity(postJson, "UTF-8");
+            httppost.setEntity(strEntity);
 
-			httppost.setHeader("Accept", "application/json");
-			httppost.setHeader("Content-Type", "application/json");
+            httppost.setHeader("Accept", "application/json");
+            httppost.setHeader("Content-Type", "application/json");
 
-			// Execute HTTP Post Request
-			HttpResponse response = httpclient.execute(httppost);
-			String result = EntityUtils.toString(response.getEntity());
-			return result;
-		} catch (Exception e) {
-			return null;
-		}
-	}
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+            String result = EntityUtils.toString(response.getEntity());
+            return result;
+        } catch (Exception e) {
+            return null;
+        }
+    }
 
-	// public static String executePostWithStringData(String postURL,
-	// String postData) {
-	// HttpURLConnection urlConnection = null;
-	// try {
-	// URL url = new URL(postURL);
-	// urlConnection = (HttpURLConnection) url.openConnection();
-	// urlConnection.addRequestProperty("Accept", "application/json");
-	//
-	// if (postData != null) {
-	// byte[] postBytes = postData.getBytes();
-	// urlConnection.addRequestProperty("Content-Type",
-	// "application/json");
-	// urlConnection.addRequestProperty("Content-Length", ""
-	// + postBytes.length);
-	// urlConnection.setDoOutput(true);
-	// urlConnection.setChunkedStreamingMode(0);
-	//
-	// OutputStream out = new BufferedOutputStream(
-	// urlConnection.getOutputStream());
-	//
-	// out.write(postData.getBytes());
-	// }
-	//
-	// InputStream in = new BufferedInputStream(
-	// urlConnection.getInputStream());
-	// InputStreamReader isr = new InputStreamReader(in);
-	// BufferedReader br = new BufferedReader(isr);
-	// String line = "";
-	// String json = "";
-	// while ((line = br.readLine()) != null) {
-	// json += line;
-	// }
-	// return json;
-	// } catch (Exception e) {
-	// Log.i("NetworkUtils", "Exception creating user");
-	// } finally {
-	// if (urlConnection != null) {
-	// urlConnection.disconnect();
-	// }
-	// }
-	// return null;
-	// }
+    // public static String executePostWithStringData(String postURL,
+    // String postData) {
+    // HttpURLConnection urlConnection = null;
+    // try {
+    // URL url = new URL(postURL);
+    // urlConnection = (HttpURLConnection) url.openConnection();
+    // urlConnection.addRequestProperty("Accept", "application/json");
+    //
+    // if (postData != null) {
+    // byte[] postBytes = postData.getBytes();
+    // urlConnection.addRequestProperty("Content-Type",
+    // "application/json");
+    // urlConnection.addRequestProperty("Content-Length", ""
+    // + postBytes.length);
+    // urlConnection.setDoOutput(true);
+    // urlConnection.setChunkedStreamingMode(0);
+    //
+    // OutputStream out = new BufferedOutputStream(
+    // urlConnection.getOutputStream());
+    //
+    // out.write(postData.getBytes());
+    // }
+    //
+    // InputStream in = new BufferedInputStream(
+    // urlConnection.getInputStream());
+    // InputStreamReader isr = new InputStreamReader(in);
+    // BufferedReader br = new BufferedReader(isr);
+    // String line = "";
+    // String json = "";
+    // while ((line = br.readLine()) != null) {
+    // json += line;
+    // }
+    // return json;
+    // } catch (Exception e) {
+    // Log.i("NetworkUtils", "Exception creating user");
+    // } finally {
+    // if (urlConnection != null) {
+    // urlConnection.disconnect();
+    // }
+    // }
+    // return null;
+    // }
 }
